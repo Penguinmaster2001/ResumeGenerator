9cf4f46afabaeabf2a71ab20f0d54120be99551e
Anthony Cieri <penguinmillion@gmail.com>
2025-03-04 00:46:26 -0800

    feat: remove skill ID, it's never used

--- src/ProjectLogging/Skills/Skill.cs
+++ src/ProjectLogging/Skills/Skill.cs
@@ -8,12 +8,4 @@ namespace ProjectLogging.Skills;
 
 
 [JsonSerializable(typeof(Skill))]
-public record Skill(string Category, string Name)
-{
-    public static int GetSkillID(string category, string name) => HashCode.Combine(category, name);
-
-
-
-    [JsonIgnore]
-    public int ID { get; init; } = GetSkillID(Category, Name);
-}
+public record Skill(string Category, string Name);
